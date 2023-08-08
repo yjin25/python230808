b3230f5c7a1298940da782dd6041afb4cf45d6c6
yjin25 <yjin25@gmail.com>
2023-08-08 16:02:39 +0900

    클래스  연습3

--- 생성자소멸자01.py
+++ 생성자소멸자01.py
@@ -1,8 +1,18 @@
 # -*- 생성자와 소멸자 -*-
 class MyClass:
+    # 초기화(생성자)
     def __init__(self, value):
         self.value = value
         print("Instace is created! value = ", value)
+    # 소멸자
     def __del__(self):
         print("Instance is deleted!")
+
+# 인스턴스 생성
+m = MyClass(5)
+#삭제
+# del m
+# 마무리
+print("전체 코드 실행 종료")
+
 
