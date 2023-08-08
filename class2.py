9915e2b7aafcaff35755889dffa7b57655a65921
yjin25 <yjin25@gmail.com>
2023-08-08 15:05:14 +0900

    오늘 수업은 클래스 연습

---
+++ class2.py
@@ -0,0 +1,15 @@
+# class2.py
+
+class Product:
+    def __init__(self, id, name, price):
+        self.id = 100
+        self.name = name
+        self.price = price
+    def print(self):
+        print("{0}, {1}, {2}".format(
+            self.id, self.name, self.price))
+        
+# 인스턴스 생성
+p1 = Product(100, "아이폰14", 950000)
+p1.print()
+
